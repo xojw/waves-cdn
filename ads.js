c9c04b275272bb4832b7ef6edca502a4d82047d7
sent <143974574+xojw@users.noreply.github.com>
2025-03-25 17:21:30 -0500

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,11 +1,11 @@
-(function() {
+(function(){
   var adScript1 = document.createElement('script');
   adScript1.type = 'text/javascript';
   adScript1.src = '//pl26200346.effectiveratecpm.com/08/db/84/08db842da9b43ad3d13c14634f9fd1c8.js';
   document.head.appendChild(adScript1);
-
   var adScript2 = document.createElement('script');
   adScript2.type = 'text/javascript';
   adScript2.src = '//pl26200262.effectiveratecpm.com/f0/e8/15/f0e81559842363ebf19aa99900ff2d02.js';
   document.head.appendChild(adScript2);
+  console.log("Ads injected.");
 })();
