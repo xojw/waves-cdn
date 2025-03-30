4d5a3097eae349ecca82436f7c84848034eb3e8a
sent <143974574+xojw@users.noreply.github.com>
2025-03-30 16:34:26 -0500

    Create ad.js

---
+++ ad.js
@@ -0,0 +1,16 @@
+var overlay = document.createElement('div');
+overlay.style.position = 'fixed';
+overlay.style.top = '0';
+overlay.style.left = '0';
+overlay.style.width = '100%';
+overlay.style.height = '100%';
+overlay.style.zIndex = '999999';
+overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+overlay.style.cursor = 'pointer';
+function handleClick() {
+  window.open("https://www.effectiveratecpm.com/jmzq26nu?key=362a2802eeb1b13df2f74ab21f27a973", '_blank');
+  overlay.removeEventListener('click', handleClick);
+  document.body.removeChild(overlay);
+}
+overlay.addEventListener('click', handleClick);
+document.body.appendChild(overlay);
